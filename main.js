86bc74802419dff35291d1b76551365cb3e9af94
diansp07 <78125195+diansp07@users.noreply.github.com>
2022-05-19 14:24:54 -0500

    Update main.js

--- main.js
+++ main.js
@@ -6,3 +6,5 @@ document.querySelector(".menu-btn").addEventListener("click", () => {
   ScrollReveal().reveal('.news-cards', { delay: 500 });
   ScrollReveal().reveal('.cards-banner-one', { delay: 500 });
   ScrollReveal().reveal('.cards-banner-two', { delay: 500 });
+
+
